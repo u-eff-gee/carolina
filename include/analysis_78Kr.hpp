1b458029936324a7d78ef8c5cbd2ffb2075539c4
Udo Friman-Gayer <ufrimangayer@gmail.com>
2022-01-06 09:25:54 -0500

    Remove event-counter module
    - The event counter had been introduced to provide a dummy counter module for all detectors that were not attached to a scaler. It is obsolete now with the introduction of the DetectorType property.

--- include/analysis_78Kr.hpp
+++ include/analysis_78Kr.hpp
@@ -10,7 +10,6 @@ using std::make_shared;
 #include "counter_detector_channel.hpp"
 #include "energy_sensitive_detector.hpp"
 #include "energy_sensitive_detector_channel.hpp"
-#include "event_counter.hpp"
 #include "mdpp16.hpp"
 #include "v830.hpp"
 
@@ -43,7 +42,6 @@ const double tdc_resolution =
 
 Analysis analysis(
     {
-        shared_ptr<ScalerModule>(new EventCounter<30>()),
         make_shared<MDPP16>("amplitude_cross", "channel_time_cross",
                             "module_timestamp_cross", tdc_resolution),
         make_shared<MDPP16>("amplitude_back", "channel_time_back",
@@ -54,23 +52,85 @@ Analysis analysis(
                             "module_timestamp_qdc", tdc_resolution),
         make_shared<V830>(5.),
     },
-    {clover, paddle, pulser},
+    {clover, clover, clover, clover, clover, coaxial, coaxial, clover,
+     fission_chamber, coaxial, paddle, pulser},
     {make_shared<EnergySensitiveDetector>(
          "clover_1",
+         vector<shared_ptr<Channel>>{
+             make_shared<EnergySensitiveDetectorChannel>("E1", 0, 0),
+             make_shared<EnergySensitiveDetectorChannel>("E2", 0, 1),
+             make_shared<EnergySensitiveDetectorChannel>("E3", 0, 2),
+             make_shared<EnergySensitiveDetectorChannel>("E4", 0, 3),
+         }),
+     make_shared<EnergySensitiveDetector>(
+         "clover_3",
+         vector<shared_ptr<Channel>>{
+             make_shared<EnergySensitiveDetectorChannel>("E1", 0, 4),
+             make_shared<EnergySensitiveDetectorChannel>("E2", 0, 5),
+             make_shared<EnergySensitiveDetectorChannel>("E3", 0, 6),
+             make_shared<EnergySensitiveDetectorChannel>("E4", 0, 7),
+         }),
+     make_shared<EnergySensitiveDetector>(
+         "clover_5",
+         vector<shared_ptr<Channel>>{
+             make_shared<EnergySensitiveDetectorChannel>("E1", 0, 8),
+             make_shared<EnergySensitiveDetectorChannel>("E2", 0, 9),
+             make_shared<EnergySensitiveDetectorChannel>("E3", 0, 10),
+             make_shared<EnergySensitiveDetectorChannel>("E4", 0, 11),
+         }),
+     make_shared<EnergySensitiveDetector>(
+         "clover_7",
+         vector<shared_ptr<Channel>>{
+             make_shared<EnergySensitiveDetectorChannel>("E1", 0, 12),
+             make_shared<EnergySensitiveDetectorChannel>("E2", 0, 13),
+             make_shared<EnergySensitiveDetectorChannel>("E3", 0, 14),
+             make_shared<EnergySensitiveDetectorChannel>("E4", 0, 15),
+         }),
+     make_shared<EnergySensitiveDetector>(
+         "clover_B1",
          vector<shared_ptr<Channel>>{
              make_shared<EnergySensitiveDetectorChannel>("E1", 1, 0),
              make_shared<EnergySensitiveDetectorChannel>("E2", 1, 1),
              make_shared<EnergySensitiveDetectorChannel>("E3", 1, 2),
              make_shared<EnergySensitiveDetectorChannel>("E4", 1, 3),
          }),
+     make_shared<EnergySensitiveDetector>(
+         "coaxial_B2",
+         vector<shared_ptr<Channel>>{
+             make_shared<EnergySensitiveDetectorChannel>("E1", 1, 4),
+         }),
+     make_shared<EnergySensitiveDetector>(
+         "coaxial_B4",
+         vector<shared_ptr<Channel>>{
+             make_shared<EnergySensitiveDetectorChannel>("E1", 1, 8),
+         }),
+     make_shared<EnergySensitiveDetector>(
+         "clover_B5",
+         vector<shared_ptr<Channel>>{
+             make_shared<EnergySensitiveDetectorChannel>("E1", 1, 12),
+             make_shared<EnergySensitiveDetectorChannel>("E2", 1, 13),
+             make_shared<EnergySensitiveDetectorChannel>("E3", 1, 14),
+             make_shared<EnergySensitiveDetectorChannel>("E4", 1, 15),
+         }),
+     make_shared<EnergySensitiveDetector>(
+         "fission_chamber",
+         vector<shared_ptr<Channel>>{
+             make_shared<EnergySensitiveDetectorChannel>("E1", 2, 12),
+             make_shared<EnergySensitiveDetectorChannel>("E2", 2, 13),
+         }),
+     make_shared<EnergySensitiveDetector>(
+         "zero_degree",
+         vector<shared_ptr<Channel>>{
+             make_shared<EnergySensitiveDetectorChannel>("E", 2, 15),
+         }),
      make_shared<CounterDetector>(
          "paddle",
          vector<shared_ptr<Channel>>{
-             make_shared<CounterDetectorChannel>("mirror", 5, 12),
-             make_shared<CounterDetectorChannel>("single", 5, 13),
-             make_shared<CounterDetectorChannel>("compton", 5, 14)}),
+             make_shared<CounterDetectorChannel>("mirror", 4, 12),
+             make_shared<CounterDetectorChannel>("single", 4, 13),
+             make_shared<CounterDetectorChannel>("compton", 4, 14)}),
      make_shared<CounterDetector>(
          "mvlc",
          vector<shared_ptr<Channel>>{
-             make_shared<CounterDetectorChannel>("pulser", 5, 15)})},
+             make_shared<CounterDetectorChannel>("pulser", 4, 15)})},
     {});
