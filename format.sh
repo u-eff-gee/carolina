036a84e9c142cf094a34dd3a5ca177af5abdcc91
Udo Friman-Gayer <ufrimangayer@gmail.com>
2022-01-10 10:58:39 -0500

    Set up formatting script for subdirectories

--- format.sh
+++ format.sh
@@ -1,2 +1,19 @@
-clang-format -i -style="{IndentWidth: 4, TabWidth: 4}" include/*.hpp
-clang-format -i -style="{IndentWidth: 4, TabWidth: 4}" source/*.cpp
+FILES=\
+`ls include/analysis/*.hpp`" "\
+`ls include/detectors/*.hpp`" "\
+`ls include/io/*.hpp`" "\
+`ls include/modules/*.hpp`" "\
+`ls include/programs/*.hpp.in`" "\
+`ls include/test/*.hpp.in`" "\
+`ls source/analysis/*.cpp`" "\
+`ls source/detectors/*.cpp`" "\
+`ls source/io/*.cpp`" "\
+`ls source/modules/*.cpp`" "\
+`ls source/programs/*.cpp`" "\
+`ls source/test/*.cpp`" "\
+
+for FILE in $FILES
+do
+    echo Formatting $FILE ...
+    clang-format -i -style="{IndentWidth: 4, TabWidth: 4}" $FILE
+done
