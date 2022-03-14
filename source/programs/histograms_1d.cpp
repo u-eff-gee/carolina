64d9495032249c3059c97ef2b4866b86a1ab98c8
Udo Friman-Gayer <ufrimangayer@gmail.com>
2022-03-14 09:09:56 -0400

    Correct indexing for addback spectra

--- source/programs/histograms_1d.cpp
+++ source/programs/histograms_1d.cpp
@@ -88,7 +88,7 @@ int main(int argc, char **argv) {
         if (analysis.energy_sensitive_detectors[n_detector_1]->channels.size() >
             1) {
             histogram_name =
-                analysis.detectors[n_detector_1]->name + "_addback";
+                analysis.energy_sensitive_detectors[n_detector_1]->name + "_addback";
             addback_histograms.push_back(new TH1D(
                 histogram_name.c_str(), histogram_name.c_str(),
                 analysis.energy_sensitive_detectors[n_detector_1]
