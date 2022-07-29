6955ab777faf242df14512c8d26c697c595b5534
Udo Friman-Gayer <ufrimangayer@gmail.com>
2022-07-29 11:09:12 -0400

    Update 64Ni experiment config
    - Binning for LaBr histograms.
    - Configuration for LaBr-LaBr and LaBr-HPGe coincidences.
    - Energy calibration.

--- include/experiments/64Ni.hpp
+++ include/experiments/64Ni.hpp
@@ -32,31 +32,36 @@ Analysis analysis(
             Histogram{8192, -4096 - 0.5, 4096 - 0.5},
             Histogram{8192, (-4096 - 0.5) * 0.125, (4096 - 0.5) * 0.125}),
         make_shared<EnergySensitiveDetectorGroup>(
-            "labr", Histogram{16384, -0.5, 16383.5},
-            Histogram{4096, -0.5, 4096 - 0.5},
+            "labr", Histogram{4096, -2, 16382},
+            Histogram{16384, -0.125, 4096 - 0.125},
             Histogram{8192, -4096 - 0.5, 4096 - 0.5},
             Histogram{8192, (-4096 - 0.5) * 0.125, (4096 - 0.5) * 0.125}),
     },
+    
     {
         make_shared<EnergySensitiveDetector>(
-            "labr_1", vector<EnergySensitiveDetectorChannel>{{"E", 0, 0}}, 1),
+            "labr_1",vector<EnergySensitiveDetectorChannel>{ {"E", 0, 0,[](const double channel, const long long) {return (17.601223374686242 +9.044300913068591 * channel * 4. +-0.0030645588610997794 * channel * channel * 16. +2.378951795388353e-06 * channel * channel *channel * 64.);}}}, 1),
         make_shared<EnergySensitiveDetector>(
-            "labr_2", vector<EnergySensitiveDetectorChannel>{{"E", 0, 1}}, 1),
+            "labr_2",vector<EnergySensitiveDetectorChannel>{{"E", 0, 1,[](const double channel, const long long) {return (3.30011886270799 +6.5018748622678295 * channel * 4 +0.0008258622567823124 * channel * channel * 16. +2.9562841744863787e-07 * channel * channel *channel * 64.);}}},1),
         make_shared<EnergySensitiveDetector>(
-            "labr_3", vector<EnergySensitiveDetectorChannel>{{"E", 0, 2}}, 1),
+            "labr_3",vector<EnergySensitiveDetectorChannel>{{"E", 0, 2,[](const double channel, const long long) {return (-4.334228864277975 +10.847058837462665 * channel * 4. +-0.00633271118898465 * channel * channel * 16. + 5.240694745133814e-06 * channel * channel * channel * 64.); }}}, 1),
         make_shared<EnergySensitiveDetector>(
-            "labr_4", vector<EnergySensitiveDetectorChannel>{{"E", 0, 3}}, 1),
+            "labr_4",vector<EnergySensitiveDetectorChannel>{{"E", 0, 3,[](const double channel, const long long) {return (3.1909193216067653 +8.571459640672055 * channel * 4. +0.0008493099811167642 * channel * channel * 16. +6.174181102734334e-09 * channel * channel *channel * 64.); }}}, 1),
+            make_shared<EnergySensitiveDetector>(
+             "hpge_1", vector<EnergySensitiveDetectorChannel>{{"E", 0, 4, [](const double channel, const long long){ return (1.57661973524373+0.6817545334755168*channel+1.3873238594790636e-07*channel*channel);}}}, 0),
         make_shared<EnergySensitiveDetector>(
-            "hpge_1", vector<EnergySensitiveDetectorChannel>{{"E", 0, 4}}, 0),
+            "hpge_2", vector<EnergySensitiveDetectorChannel>{{"E", 0, 5, [](const double channel, const long long){ return (-0.12920658259438178+0.6949954832528151*channel+ -3.8910539767706286e-08*channel*channel);}}}, 0),
         make_shared<EnergySensitiveDetector>(
-            "hpge_2", vector<EnergySensitiveDetectorChannel>{{"E", 0, 5}}, 0),
+            "hpge_3", vector<EnergySensitiveDetectorChannel>{{"E", 0, 6, [](const double channel, const long long){ return (2.3379527308416117+0.728372161229253*channel+1.6704897670007945e-07*channel*channel);}}}, 0),
         make_shared<EnergySensitiveDetector>(
-            "hpge_3", vector<EnergySensitiveDetectorChannel>{{"E", 0, 6}}, 0),
-        make_shared<EnergySensitiveDetector>(
-            "hpge_4", vector<EnergySensitiveDetectorChannel>{{"E", 0, 7}}, 0),
+            "hpge_4", vector<EnergySensitiveDetectorChannel>{{"E", 0, 7, [](const double channel, const long long){ return (-0.3672812348956054+0.7588636040581321*channel+-1.2367294537567397e-07*channel*channel);}}}, 0),
     },
-    {{"labr_1_vs_labr_2",
-      {0},
-      {1},
-      Histogram{4096, -0.5, 4096 - 0.5},
-      Histogram{4096, -0.5, 4096 - 0.5}}});
+    {{"labr_labr",
+      {0, 1, 2, 3}, {},
+      Histogram{4096, -2, 16384 - 2},
+      Histogram{4096, -2, 16384 - 2}},
+      {"labr_hpge",
+      {0, 1, 2, 3}, {4, 5, 6, 7},
+      Histogram{4096, -2, 16384 - 2},
+      Histogram{16384, -0.5, 16384 - 0.5}},
+      });
